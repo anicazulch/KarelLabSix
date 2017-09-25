aa17921a4468ef87fdbda938725315b331ae1c0c
anicazulch <31353283+anicazulch@users.noreply.github.com>
2017-09-25 11:58:13 -0700

    Lab 6 Day 1

--- BeeperBot.java
+++ BeeperBot.java
@@ -3,7 +3,7 @@ import kareltherobot.*;
 
 
 /**
- * @author :  D. Appel
+ * @author :  A. Zulch
  */
 public class BeeperBot extends Robot
 {
@@ -16,13 +16,57 @@ public class BeeperBot extends Robot
     }
     
     public void findAndCountBeepers() {
-        
+        while(frontIsClear()) {
+            collectBeeperSteeple();
+        }
     }
     
     public int[] getBeeperList() {
             //This will be used to print the beepers found.  No need to modify.
             return beeperList;
     }
-   
+    
+    public void collectBeeperSteeple() {
+        findBeepers();
+    }
+    public void findBeepers() {
+        while(!nextToABeeper() && frontIsClear()){
+           move(); 
+        }
+        pickUpColumn();
+        goDown();
+        faceEast();
+        }
+    public void pickUpColumn(){
+        tempCount = 0;
+        while(nextToABeeper()){
+            faceNorth();
+            pickBeeper();
+            tempCount++;
+            move();
+        }
+        beeperList[0] = tempCount;
+    }
+    public void goDown(){
+        while(frontIsClear()){
+            faceSouth();
+            move();
+        }
+    }
+    public void faceEast() {
+        while(!facingEast()){
+            turnLeft();
+        }
+    }
+    public void faceSouth() {
+        while(!facingSouth()){
+            turnLeft();
+        }
+    }
+    public void faceNorth() {
+        while(!facingNorth()) {
+            turnLeft();
+        }
+    }
 }
 
